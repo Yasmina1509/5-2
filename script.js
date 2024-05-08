e2fec9240074b47dcd802003241432e38bac3f4c
Yasmina1509 <xikmatullaevayasmina115@gmail.com>
2024-05-08 13:40:03 +0500

    5.2

---
+++ script.js
@@ -0,0 +1,13 @@
+let myname = {
+    name: `yasmina`,
+    id: 682039,
+    password: 58406,
+}
+
+let name = prompt("Введите своё имя:"); 
+let id = prompt("Введите ID:"); 
+let password = prompt("Введите пароль:"); 
+let inforaboutme = { имя: name, идентификатор: id, пароль: password }; 
+console.log(myname)
+console.log(inforaboutme);
+
